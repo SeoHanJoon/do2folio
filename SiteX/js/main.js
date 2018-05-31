81e923aed6134b4d2c9cc75c5b7e6178aa2d7d9f
Jonghwan <jh.wwwon@daum.net>
2018-05-31 23:11:59 +0900

    resposive 완성, js 추가 (project img 사이즈 조정, float btn)

--- SiteX/js/main.js
+++ SiteX/js/main.js
@@ -22,3 +22,32 @@ window.addEventListener("scroll", stickyNavigation);
         menuClasses.toggle('active');
     }
 })();
+
+function resizeSlider() {
+    const slider = document.querySelector('.slider');
+    const windowHeight = window.innerHeight;
+
+}
+
+function fitHeightToWidth() {
+    const projects = document.querySelectorAll('.portfolio__project');
+
+    // for (let i = 0; i < projects.length; i++) {
+    //     projects[i].style.height = projects[i].clientWidth + 'px';
+    // }
+
+    for (const iterator of projects) {
+        iterator.style.height = iterator.clientWidth + 'px';
+    }
+}
+
+fitHeightToWidth();
+window.addEventListener("resize", fitHeightToWidth);
+
+const floatBtn = document.querySelector('.float-btn--circle');
+floatBtn.onclick = function () {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
