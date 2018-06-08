eaa00bb65fb1cb61f2f28876d0f8072e1cec0aff
Jonghwan <jh.wwwon@daum.net>
2018-06-08 13:37:26 +0900

    add new web 'grand gallery'

---
+++ public/grand/js/main.js
@@ -0,0 +1,9 @@
+const body = document.body;
+const wrapper = document.getElementById("wrapper");
+
+body.classList.add("load");
+wrapper.style.height = window.innerHeight + "px";
+
+// window.onload = () => {
+//   body.classList.add("load");
+// };
